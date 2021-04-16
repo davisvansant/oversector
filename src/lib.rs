9d267e3ff5873f49f05132596b51f03e9353ba10
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-16 13:47:57 -0700

    initial v1 enum, struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,35 @@
-// unimplemented!()
+use std::ffi::OsString;
+use std::path::PathBuf;
+
+pub enum V1controller {
+    Cpu,
+    Cpuaccount,
+    Cpuset,
+    Memory,
+    Devices,
+    Freezer,
+    NetCls,
+    Blkio,
+    PerfEvent,
+    NetPrio,
+    Hugetlb,
+    Pids,
+    Rdma,
+}
+
+pub struct Cgroup {
+    pub filesystem: PathBuf,
+}
+
+impl Cgroup {
+    pub async fn init() -> Cgroup {
+        let os_string = OsString::from("/sys/fs/cgroup/");
+        let mut path_buffer = PathBuf::new();
+
+        path_buffer.push(os_string);
+
+        Cgroup {
+            filesystem: path_buffer,
+        }
+    }
+}
