6d9734381825520e9db5d216cc70285a92448433
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-16 17:34:47 -0700

    initial collect_cpu, basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::ffi::OsString;
 use std::path::PathBuf;
 
+use tokio::fs::read_dir;
+// use tokio::fs::File;
+// use tokio::io::AsyncReadExt;
+
 pub enum V1controller {
     Cpu,
     Cpuaccount,
@@ -31,5 +35,42 @@ impl Cgroup {
         Cgroup {
             filesystem: path_buffer,
         }
+    }
+
+    pub async fn collect_cpu(&mut self) {
+        let cpu_os_string = OsString::from("cpu/");
+        let cpu_path = self.filesystem.join(cpu_os_string);
+        let read_dir = read_dir(&cpu_path).await;
+
+        match read_dir {
+            Ok(mut contents) => {
+                while let Ok(entries) = contents.next_entry().await {
+                    if let Some(entry) = entries {
+                        println!("File name - {:?}", entry.file_name());
+                        println!("Path - {:?}", entry.path());
+                    } else {
+                        break;
+                    }
+                }
+            }
+            Err(error) => println!("{:?}", error),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn init() {
+        let cgroup = Cgroup::init().await;
+        assert_eq!(cgroup.filesystem.is_dir(), true);
+    }
+
+    #[tokio::test]
+    async fn collect_cpu() {
+        let mut cgroup = Cgroup::init().await;
+        cgroup.collect_cpu().await;
     }
 }
