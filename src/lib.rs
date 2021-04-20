0492f470b880d544e12ca922f8c345f7c55c33ff
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-20 12:00:19 -0700

    adjust collect_perfevent test

--- src/lib.rs
+++ src/lib.rs
@@ -265,11 +265,15 @@ mod tests {
 
     #[tokio::test]
     async fn collect_perfevent() {
-        let mut test_cgroup = Cgroup::init().await;
+        let test_cgroup = Cgroup::init().await;
         let test_perfevent_controller = V1controller::PerfEvent;
-        test_cgroup
-            .collect_controller(test_perfevent_controller)
-            .await;
+        let mut test_subsystem = Subsystem::init(&test_cgroup).await;
+        assert_eq!(test_subsystem.state.len(), 0);
+        assert_eq!(test_subsystem.hierarchy, PathBuf::from("/sys/fs/cgroup/"));
+        test_subsystem.collect(&test_perfevent_controller).await;
+        assert_eq!(test_subsystem.state.len(), 1);
+        test_subsystem.collect(&test_perfevent_controller).await;
+        assert_eq!(test_subsystem.state.len(), 2);
     }
 
     #[tokio::test]
