55bafab00be8ffa3d0950c3a8320fd0b54b8877b
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-20 12:11:23 -0700

    adjust collect_hugetlb test

--- src/lib.rs
+++ src/lib.rs
@@ -291,11 +291,15 @@ mod tests {
 
     #[tokio::test]
     async fn collect_hugetlb() {
-        let mut test_cgroup = Cgroup::init().await;
+        let test_cgroup = Cgroup::init().await;
         let test_hugetlb_controller = V1controller::Hugetlb;
-        test_cgroup
-            .collect_controller(test_hugetlb_controller)
-            .await;
+        let mut test_subsystem = Subsystem::init(&test_cgroup).await;
+        assert_eq!(test_subsystem.state.len(), 0);
+        assert_eq!(test_subsystem.hierarchy, PathBuf::from("/sys/fs/cgroup/"));
+        test_subsystem.collect(&test_hugetlb_controller).await;
+        assert_eq!(test_subsystem.state.len(), 1);
+        test_subsystem.collect(&test_hugetlb_controller).await;
+        assert_eq!(test_subsystem.state.len(), 2);
     }
 
     #[tokio::test]
