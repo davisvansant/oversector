43b2eb71d3a5f0a3573210076fc5cc2de7a922f6
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-20 12:43:55 -0700

    cleanup and remove collect_controller() from Cgroup

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@ use std::path::PathBuf;
 
 use tokio::fs::read;
 use tokio::fs::read_dir;
-use tokio::fs::read_to_string;
-// use tokio::fs::File;
-// use tokio::io::AsyncReadExt;
 
 pub enum V1controller {
     Cpu,
@@ -36,46 +33,6 @@ impl Cgroup {
 
         Cgroup {
             filesystem: path_buffer,
-        }
-    }
-
-    pub async fn collect_controller(&mut self, controller: V1controller) {
-        let os_string = match controller {
-            V1controller::Cpu => OsString::from("cpu/"),
-            V1controller::Cpuacct => OsString::from("cpuacct/"),
-            V1controller::Cpuset => OsString::from("cpuset/"),
-            V1controller::Memory => OsString::from("memory/"),
-            V1controller::Devices => OsString::from("devices/"),
-            V1controller::Freezer => OsString::from("freezer/"),
-            V1controller::NetCls => OsString::from("net_cls/"),
-            V1controller::Blkio => OsString::from("blkio/"),
-            V1controller::PerfEvent => OsString::from("perf_event/"),
-            V1controller::NetPrio => OsString::from("net_prio/"),
-            V1controller::Hugetlb => OsString::from("hugetlb/"),
-            V1controller::Pids => OsString::from("pids/"),
-            V1controller::Rdma => OsString::from("rdma/"),
-        };
-
-        let path = self.filesystem.join(os_string);
-        let read_dir = read_dir(&path).await;
-
-        match read_dir {
-            Ok(mut contents) => {
-                while let Ok(entries) = contents.next_entry().await {
-                    if let Some(entry) = entries {
-                        println!("File name - {:?}", entry.file_name());
-                        println!("Path - {:?}", entry.path());
-                        let contents = read_to_string(entry.path().as_path()).await;
-                        match contents {
-                            Ok(content) => println!("{}", content),
-                            Err(error) => println!("{:?}", error),
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-            Err(error) => println!("{:?}", error),
         }
     }
 }
