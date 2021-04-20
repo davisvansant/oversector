8333c375a5dc88f4f6d2f3b2cc89a91b7e61ef4f
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-20 11:54:41 -0700

    adjust collect_freezer test

--- src/lib.rs
+++ src/lib.rs
@@ -226,11 +226,15 @@ mod tests {
 
     #[tokio::test]
     async fn collect_freezer() {
-        let mut test_cgroup = Cgroup::init().await;
+        let test_cgroup = Cgroup::init().await;
         let test_freezer_controller = V1controller::Freezer;
-        test_cgroup
-            .collect_controller(test_freezer_controller)
-            .await;
+        let mut test_subsystem = Subsystem::init(&test_cgroup).await;
+        assert_eq!(test_subsystem.state.len(), 0);
+        assert_eq!(test_subsystem.hierarchy, PathBuf::from("/sys/fs/cgroup/"));
+        test_subsystem.collect(&test_freezer_controller).await;
+        assert_eq!(test_subsystem.state.len(), 1);
+        test_subsystem.collect(&test_freezer_controller).await;
+        assert_eq!(test_subsystem.state.len(), 2);
     }
 
     #[tokio::test]
